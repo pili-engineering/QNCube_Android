40caa36ff41ca26326a0035aae44c1247081e406
manjiale <1013666753@qq.com>
2022-07-05 16:07:37 +0800

    低代码添加美颜

---
+++ module_user/src/main/java/com/qiniudemo/module/user/NewVersionInfo.java
@@ -0,0 +1,9 @@
+package com.qiniudemo.module.user;
+
+public class NewVersionInfo {
+    public String version = "3.0.1";
+    public String msg = "添加美颜sdk";
+    public String packagePage = "http://fir.qnsdk.com/s6py";
+    public String packageUrl = "http://fir.qnsdk.com/s6py";
+    public String arch = "android";
+}
