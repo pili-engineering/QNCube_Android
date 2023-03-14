f4b1980e2ad40c7cc4f604074d9a90081568c96e
manjiale <1013666753@qq.com>
2023-03-14 17:31:32 +0800

    升级im提升稳定性

--- BzMudule/comp_imadapter/src/main/java/com/qiniu/qnim/QNIMManager.kt
+++ BzMudule/comp_imadapter/src/main/java/com/qiniu/qnim/QNIMManager.kt
@@ -1,6 +1,8 @@
 package com.qiniu.qnim
 
 import android.content.Context
+import android.os.Environment
+import android.util.Log
 import com.niucube.rtm.RtmManager
 import im.floo.floolib.BMXClientType
 import im.floo.floolib.BMXLogLevel
@@ -12,9 +14,24 @@ object QNIMManager {
 
     val mRtmAdapter by lazy { QNIMAdapter() }
 
+    private fun getFilesPath(context: Context): String {
+        val filePath: String =
+            if ((Environment.MEDIA_MOUNTED == Environment.getExternalStorageState() || !Environment.isExternalStorageRemovable())
+            ) {
+                Log.d("data_dir", "外部存储可用")
+                //外部存储可用
+                context.getExternalFilesDir(null)!!.path
+            } else {
+                Log.d("data_dir", "外部存储不可用")
+                //外部存储不可用
+                context.filesDir.path
+            }
+        return filePath
+    }
+
     fun init(appId: String, context: Context, config: BMXSDKConfig? = null) {
         mRtmAdapter.init(if (config == null) {
-            val appPath = context.filesDir.path
+            val appPath = getFilesPath(context)
             val dataPath = File("$appPath/data_dir")
             val cachePath = File("$appPath/cache_dir")
             dataPath.mkdirs()
@@ -35,7 +52,7 @@ object QNIMManager {
         RtmManager.setRtmAdapter(mRtmAdapter)
     }
 
-    fun unInit(){
+    fun unInit() {
         mRtmAdapter.unInit()
     }
 }
